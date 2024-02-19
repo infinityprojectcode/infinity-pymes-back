df0293e73fd065cd82074dec36cd3c1b7cd2a454
= <daniel20025febrero29@gmail.com>
2024-02-19 14:56:29 -0500

    First endpoint

--- djg_api_rest/wsgi.py
+++ djg_api_rest/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for infinitPymes project.
+WSGI config for djg_api_rest project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infinitPymes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djg_api_rest.settings')
 
 application = get_wsgi_application()
