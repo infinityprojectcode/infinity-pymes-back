749338063499ac5d9ed94d67fd2e7799d2d97c40
Daniel Muñoz <daniel20025febrero29@gmail.com>
2025-01-15 20:35:37 -0500

    Init config back

--- projects/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .api import ProjectViewSet
-
-router = routers.DefaultRouter()
-
-router.register('api/projects', ProjectViewSet, 'project')
-
-urlpatterns = router.urls
