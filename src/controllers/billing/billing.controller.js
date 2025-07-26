f0df7beb787adc33102050117487a5d1a00b2d48
Deymer Ospina <deymer.ospina@gmail.com>
2025-07-26 05:35:48 -0500

    Feat (structure): Add new controllers organized by domain with RESTful routes
    Controllers have been reorganized into folders based on their functional domain (auth, billing, checkout, products, suppliers, etc.), improving project structure and maintainability.
    
    The routes in `route.js` have also been updated to adapt to the new folder system and a cleaner RESTful convention.
    
    The internal logic of the existing controllers has not been modified other than the names of their functions and tables in the query.

---
+++ src/controllers/billing/billing.controller.js
@@ -0,0 +1,103 @@
+import getConnection from "../../database/connection.mysql.js"
+import { variablesDB } from "../../utils/params/const.database.js"
+import { responseQueries } from "../../common/enum/queries/response.queries.js"
+
+// Get data from the table
+export const getBilling = async (req, res) => {
+    const conn = await getConnection();
+    const db = variablesDB.data_base;
+    const query = `
+    SELECT * FROM ${db}.Billing`;
+    const select = await conn.query(query);
+    if (!select) return res.json({
+        status: 500,
+        message: 'Error obteniendo los datos'
+    });
+    return res.json(select[0]);
+}
+
+// Save data to the table
+export const saveBilling = async (req, res) => {
+    const { column1, column2 } = req.body;
+
+    if (!column1 || !column2) {
+        return res.json(responseQueries.error({ message: "Datos incompletos" }));
+    }
+
+    const conn = await getConnection();
+    const db = variablesDB.data_base;
+
+    const insert = await conn.query(
+        `INSERT INTO ${db}.Billing (column1, column2) VALUES (?, ?)`,
+        [column1, column2]
+    );
+
+    if (!insert) return res.json(responseQueries.error({ message: "Error al guardar los datos" }));
+
+    return res.json(responseQueries.success({ message: "Datos guardados con éxito" }));
+};
+
+// Update table data
+export const updateBilling = async (req, res) => {
+    // Depending on how the ID is obtained, whether by URL or from the body, it is saved in a variable in a different way.
+
+    // From URL
+    // const { id } = req.params;
+
+    // From BODY
+    const { id, column1, column2 } = req.body;
+
+    if (!id || !column1 || !column2) {
+        return res.json(responseQueries.error({ message: "Datos incompletos" }));
+    }
+
+    try {
+        const conn = await getConnection();
+        const db = variablesDB.data_base;
+
+        const update = await conn.query(
+            `UPDATE ${db}.Billing SET column1 = ?, column2 = ? WHERE id = ?`,
+            [column1, column2, id]
+        );
+
+        if (update.affectedRows === 0) {
+            return res.json(responseQueries.error({ message: "No se encontró el ID" }));
+        }
+
+        return res.json(responseQueries.success({ message: "Datos actualizados con éxito" }));
+    } catch (error) {
+        return res.json(responseQueries.error({ message: "Error al actualizar los datos", error }));
+    }
+};
+
+// Delete data from the table
+export const deleteBilling = async (req, res) => {
+    // From URL
+    // const { id } = req.params;
+
+    // From BODY
+    const { id } = req.body;
+
+    if (!id) {
+        return res.json(responseQueries.error({ message: "Datos incompletos" }));
+    }
+    try {
+        const conn = await getConnection();
+        const db = variablesDB.data_base;
+
+        const deleteQuery = `
+            DELETE FROM ${db}.Billing WHERE id = ?;
+        `;
+
+        const [result] = await conn.query(deleteQuery, [id]);
+
+        if (result.affectedRows === 0) {
+            return res.json(responseQueries.error({ message: "No se encontró el ID o el ID no es válido o inexistente" }));
+        }
+
+        return res.json(responseQueries.success({ message: "Datos eliminados con éxito" }));
+    } catch (error) {
+        console.error("Error al eliminar los datos: ", error);
+        return res.json(responseQueries.error({ message: "Error interno del servidor" }));
+    }
+}
---
+++ src/controllers/suppliers/supplier-contacts.controller.js
@@ -0,0 +1,103 @@
+import getConnection from "../../database/connection.mysql.js"
+import { variablesDB } from "../../utils/params/const.database.js"
+import { responseQueries } from "../../common/enum/queries/response.queries.js"
+
+// Get data from the table
+export const getSupplierContacts = async (req, res) => {
+    const conn = await getConnection();
+    const db = variablesDB.data_base;
+    const query = `
+    SELECT * FROM ${db}.SupplierContacts`;
+    const select = await conn.query(query);
+    if (!select) return res.json({
+        status: 500,
+        message: 'Error obteniendo los datos'
+    });
+    return res.json(select[0]);
+}
+
+// Save data to the table
+export const saveSupplierContacts = async (req, res) => {
+    const { column1, column2 } = req.body;
+
+    if (!column1 || !column2) {
+        return res.json(responseQueries.error({ message: "Datos incompletos" }));
+    }
+
+    const conn = await getConnection();
+    const db = variablesDB.data_base;
+
+    const insert = await conn.query(
+        `INSERT INTO ${db}.SupplierContacts (column1, column2) VALUES (?, ?)`,
+        [column1, column2]
+    );
+
+    if (!insert) return res.json(responseQueries.error({ message: "Error al guardar los datos" }));
+
+    return res.json(responseQueries.success({ message: "Datos guardados con éxito" }));
+};
+
+// Update table data
+export const updateSupplierContacts = async (req, res) => {
+    // Depending on how the ID is obtained, whether by URL or from the body, it is saved in a variable in a different way.
+
+    // From URL
+    // const { id } = req.params;
+
+    // From BODY
+    const { id, column1, column2 } = req.body;
+
+    if (!id || !column1 || !column2) {
+        return res.json(responseQueries.error({ message: "Datos incompletos" }));
+    }
+
+    try {
+        const conn = await getConnection();
+        const db = variablesDB.data_base;
+
+        const update = await conn.query(
+            `UPDATE ${db}.SupplierContacts SET column1 = ?, column2 = ? WHERE id = ?`,
+            [column1, column2, id]
+        );
+
+        if (update.affectedRows === 0) {
+            return res.json(responseQueries.error({ message: "No se encontró el ID" }));
+        }
+
+        return res.json(responseQueries.success({ message: "Datos actualizados con éxito" }));
+    } catch (error) {
+        return res.json(responseQueries.error({ message: "Error al actualizar los datos", error }));
+    }
+};
+
+// Delete data from the table
+export const deleteSupplierContacts = async (req, res) => {
+    // From URL
+    // const { id } = req.params;
+
+    // From BODY
+    const { id } = req.body;
+
+    if (!id) {
+        return res.json(responseQueries.error({ message: "Datos incompletos" }));
+    }
+    try {
+        const conn = await getConnection();
+        const db = variablesDB.data_base;
+
+        const deleteQuery = `
+            DELETE FROM ${db}.SupplierContacts WHERE id = ?;
+        `;
+
+        const [result] = await conn.query(deleteQuery, [id]);
+
+        if (result.affectedRows === 0) {
+            return res.json(responseQueries.error({ message: "No se encontró el ID o el ID no es válido o inexistente" }));
+        }
+
+        return res.json(responseQueries.success({ message: "Datos eliminados con éxito" }));
+    } catch (error) {
+        console.error("Error al eliminar los datos: ", error);
+        return res.json(responseQueries.error({ message: "Error interno del servidor" }));
+    }
+}
