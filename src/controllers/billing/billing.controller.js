740f1a12c99dc16c866f62ac8ddf87e438734b3b
Deymer Ospina <deymer.ospina@gmail.com>
2025-08-14 04:14:16 -0500

    Creation of endpoint for billing

--- src/controllers/billing/billing.controller.js
+++ src/controllers/billing/billing.controller.js
@@ -3,11 +3,29 @@ import { variablesDB } from "../../utils/params/const.database.js"
 import { responseQueries } from "../../common/enum/queries/response.queries.js"
 
 // Get data from the table
+// NOTA: El id del where se cambia dependiendo de que usuario es quien consulta, por el momento es 1 pero debe cambiarse a una variable dinámica
 export const getBilling = async (req, res) => {
     const conn = await getConnection();
     const db = variablesDB.database;
+
     const query = `
-    SELECT * FROM ${db}.Billing`;
+    SELECT
+        b.id AS billing_id,
+        CONCAT('FAC-', YEAR(NOW()), '-', LPAD(b.id, 3, '0')) AS code,
+        c.name AS customer_name,
+        DATE_FORMAT(b.created_at, '%Y-%m-%d %H:%i:%s') AS billing_date,
+        DATE_FORMAT(b.expiration_at, '%Y-%m-%d %H:%i:%s') AS billing_expiration,
+        SUM(bd.quantity) as total_consumption,
+        SUM(bd.subtotal) as total_price,
+        sb.name_state
+    FROM ${db}.billing b
+    JOIN ${db}.customers c ON b.customer_id  = c.id
+    JOIN ${db}.state_billing sb ON b.state_billing_id = sb.id
+    JOIN ${db}.billing_detail bd ON b.id = bd.billing_id
+    WHERE b.business_id = 1
+    GROUP BY b.id, c.name, b.created_at , b.expiration_at, sb.name_state
+    ORDER BY b.id ASC;
+    `;
     const select = await conn.query(query);
     if (!select) return res.json({
         status: 500,
@@ -28,7 +46,7 @@ export const saveBilling = async (req, res) => {
     const db = variablesDB.database;
 
     const insert = await conn.query(
-        `INSERT INTO ${db}.Billing (column1, column2) VALUES (?, ?)`,
+        `INSERT INTO ${db}.billing (column1, column2) VALUES (?, ?)`,
         [column1, column2]
     );
 
@@ -56,7 +74,7 @@ export const updateBilling = async (req, res) => {
         const db = variablesDB.database;
 
         const update = await conn.query(
-            `UPDATE ${db}.Billing SET column1 = ?, column2 = ? WHERE id = ?`,
+            `UPDATE ${db}.billing SET column1 = ?, column2 = ? WHERE id = ?`,
             [column1, column2, id]
         );
 
@@ -86,7 +104,7 @@ export const deleteBilling = async (req, res) => {
         const db = variablesDB.database;
 
         const deleteQuery = `
-            DELETE FROM ${db}.Billing WHERE id = ?;
+            DELETE FROM ${db}.billing WHERE id = ?;
         `;
 
         const [result] = await conn.query(deleteQuery, [id]);
